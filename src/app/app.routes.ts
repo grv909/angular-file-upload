4ab551315c92d6447ab5e6d1bdcb16929e78a8f0
gaurav <gaurav.909.iitr@gmail.com>
2026-03-21 10:03:22 +0530

    Initial project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FileUpload } from './file-upload/file-upload';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: FileUpload,
+  },
+];
